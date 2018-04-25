f2bb69ef622e89e677489db401c4e8133d7d4178
burdoto <burdoto@outlook.com>
2018-04-25 14:22:13 +0200

    Added UserRecord Class for a Future Release
    It will keep Record of how many Messages what user sent in  the last week, and how long the average Message was.

---
+++ src/main/java/de/kaleidox/dangobot/bot/specific/UserRecord.java
@@ -0,0 +1,49 @@
+package de.kaleidox.dangobot.bot.specific;
+
+import de.kaleidox.dangobot.util.Debugger;
+import de.kaleidox.dangobot.util.Mapper;
+import de.kaleidox.dangobot.util.serializer.PropertiesMapper;
+import org.javacord.api.entity.server.Server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UserRecord {
+    private static final ConcurrentHashMap<Server, UserRecord> selfMap = new ConcurrentHashMap<>();
+    private Debugger log = new Debugger(UserRecord.class.getName());
+    private Server myServer;
+    private Long serverId;
+    private PropertiesMapper values;
+
+    private UserRecord(Server server) {
+        this.myServer = server;
+
+        serverId = myServer.getId();
+
+        File records = new File("props/userRecords/" + serverId + ".properties");
+
+        if (!records.exists()) {
+            try {
+                records.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.values = new PropertiesMapper(records, ';');
+
+        Mapper.safePut(selfMap, server, this);
+    }
+
+    /*
+    This Object's "Constructor".
+    Looks for an already existing instance of the required Object or creates a new one.
+
+    @returns The adequate Instance of this Object.
+     */
+    public static UserRecord softGet(Server server) {
+        return (selfMap.containsKey(server) ? selfMap.get(server) : selfMap.put(server, new UserRecord(server)));
+    }
+
+    // TODO Scheduled to a later release
+}
